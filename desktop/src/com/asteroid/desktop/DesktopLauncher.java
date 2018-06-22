95c9a7eb1b5b07051a14a045fd5ed770596439dc
Michał Woźniak <mw3f@tlen.pl>
2018-06-22 13:43:17 +0200

    Initial project

---
+++ desktop/src/com/asteroid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asteroid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asteroid.Asteroid;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Asteroid(), config);
+	}
+}
